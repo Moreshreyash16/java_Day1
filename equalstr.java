d7758dbcb71d21926b2337dd0067e7176d1e997e
Moreshreyash16 <129577115+Moreshreyash16@users.noreply.github.com>
2023-04-19 23:10:21 +0530

    Add files via upload

---
+++ equalstr.java
@@ -0,0 +1,14 @@
+package Bridgelabz;
+
+public class equalstr {
+    public static void main(String [] args){
+        String s1="Shreyash";
+        String s2="shreyash";
+        if (s1==s2){
+            System.out.println("strings are equal");
+        }
+        else {
+            System.out.println("strings are not equal");
+        }
+    }
+}
---
+++ inputname.java
@@ -0,0 +1,8 @@
+package Bridgelabz;
+
+import java.util.Scanner;
+
+public class inputname {
+    public static void main(String args[]){
+    }
+}
